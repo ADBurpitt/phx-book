134b88df0b9817669334028f0ec6723406353332
aaron <aaron.burpitt@arachnys.com>
2019-02-14 19:34:48 +0000

    idk

--- rumbl/lib/rumbl/accounts/accounts.change3.ex
+++ rumbl/lib/rumbl/accounts/accounts.change3.ex
@@ -7,11 +7,19 @@ defmodule Rumbl.Accounts do
   alias Rumbl.Accounts.User
 
   def get_user(id), do: Repo.get(User, id)
-  
+
   def get_user!(id), do: Repo.get!(User, id)
 
   def get_user_by(params), do: Repo.get_by(User, params)
 
   def list_users, do: Repo.all(User)
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert
+  end
+
+  def change_user(%User{} = user), do: User.changeset(user, %{})
 
 end
